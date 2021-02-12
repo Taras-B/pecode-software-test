913108fb9b55a527b2106434621c028972249e36
Taras-B <taras5475@gmail.com>
2021-02-12 13:00:29 +0200

    Create api and types response

---
+++ src/api/index.ts
@@ -0,0 +1,25 @@
+import { ICharacterData, IInfoAndPagination } from '../store/types'
+
+const BASE_URL = {
+  characters: 'https://rickandmortyapi.com/api/character',
+  locations: 'https://rickandmortyapi.com/api/location',
+  episodes: 'https://rickandmortyapi.com/api/episode',
+}
+
+export const rickAPI = {
+  getCharacters(): Promise<IInfoAndPagination | ICharacterData> {
+    return fetch(BASE_URL.characters)
+      .then((res) => res.json())
+      .catch((e) => console.log(e))
+  },
+  getLocations() {
+    return fetch(BASE_URL.locations)
+      .then((res) => res.json())
+      .catch((e) => console.log(e))
+  },
+  getEpisodes() {
+    return fetch(BASE_URL.episodes)
+      .then((res) => res.json())
+      .catch((e) => console.log(e))
+  },
+}
---
+++ src/store/types.ts
@@ -0,0 +1,41 @@
+export interface IInfoAndPagination {
+  count: number
+  pages: number
+  next: null | string
+  prev: null | string
+}
+
+interface ICommonData {
+  id: number
+  name: string
+  url: string
+  created: string
+}
+export interface ICharacterData extends ICommonData {
+  status: string
+  species: string
+  type: string
+  gender: string
+  origin: {
+    name: string
+    url: string
+  }
+  location: {
+    name: string
+    url: string
+  }
+  image: string
+  episode: string[]
+}
+
+export interface ILocationData extends ICommonData {
+  type: string
+  dimension: string
+  residents: string[]
+}
+
+export interface IEpisodeData extends ICommonData {
+  air_date: string
+  episode: string
+  characters: string[]
+}
